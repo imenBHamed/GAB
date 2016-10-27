7e03becfd852b809f7f2a4623d560ef6d4706b45
imenBHamed <imen.benhamed123@gmail.com>
2016-10-27 12:39:06 -0400

    ajout test creer_compte

---
+++ test/creer_compte_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+require 'GAB' 
+
+describe GAB do
+  describe ".creer" do
+    
+    describe "cas avec aucune compte a creer" do
+      it "retourne [] " do
+        GAB.creer( nil, nil,nil, nil,nil, 'bin/db.txt')
+          .must_be_empty
+      end
+    end
+    
+    describe "cas avec administrateur invalides" do
+      it "retourne un message d'erreur contenant'Vous n'avez pas l''autorisation d''ajouter un nouveau compte' " do
+        GAB.creer("alex", "alex21","alesx21_lio","admin","admin1",'bin/db.txt')
+          .must_equal "Vous n'avez pas l''autorisation d''ajouter un nouveau compte"
+      end
+    end
+  end
+end
