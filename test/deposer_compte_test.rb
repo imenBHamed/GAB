f871fdf0b56229c433282779c6641de1c79f76d2
imenBHamed <imen.benhamed123@gmail.com>
2016-10-27 15:45:49 -0400

    ajout test deposer_argent

--- test/deposer_compte_test.rb
+++ test/deposer_compte_test.rb
@@ -1,2 +1,14 @@
 require 'test_helper'
-require 'GAB' 
+require 'GAB'
+
+describe GAB do
+  describe ".creer" do
+    
+    describe "cas avec inexistance des arguments" do
+      it "retourne [] " do
+        GAB.deposer( nil, nil,'bin/db.txt',nil)
+          .must_be_empty
+      end
+    end
+  end
+end
