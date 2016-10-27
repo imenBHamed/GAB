2acbdec35fe78cd3b94f7f537b38b76486fc5e8c
imenBHamed <imen.benhamed123@gmail.com>
2016-10-27 15:19:52 -0400

    ajout test creer_compte

--- test/creer_compte_test.rb
+++ test/creer_compte_test.rb
@@ -17,5 +17,13 @@ describe GAB do
           .must_equal "Vous n'avez pas l''autorisation d''ajouter un nouveau compte"
       end
     end
+    
+     describe "cas avec client deja existe dans la base de donnees" do
+      it "retourne un message d'erreur contenant'Utilisateur deja existe dans la base de donnees' " do
+        GAB.creer("alexis", "alexis1","alexis_bush","admin","admin1",'bin/db.txt')
+          .must_equal "Vous n'avez pas l''autorisation d''ajouter un nouveau compte"
+      end
+    end
+    
   end
 end
