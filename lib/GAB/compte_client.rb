524daf181a0f9ec5139a372ebf4e352008aa3532
imenBHamed <imen.benhamed123@gmail.com>
2016-11-03 21:02:13 -0400

    ajout fonction ==

--- lib/GAB/compte_client.rb
+++ lib/GAB/compte_client.rb
@@ -11,4 +11,9 @@ class CompteClient
 	def initialize(identifiant_client, pwd_client, nom_client, argent)
 	  @identifiant_client, @pwd_client, @nom_client, @argent = identifiant_client, pwd_client, nom_client, argent
 	end
+	
+	def == (client)
+	  return  self.identifiant_client == client.identifiant_client && self.pwd_client == client.pwd_client && self.nom_client == client.nom_client && self.argent == client.argent
+	 end
+	  
 end 
